23ec39ae43ada012c88f379d781f7961b99c787e
Ziwen <zi.w.wang@mail.mcgill.com>
2019-03-22 18:20:36 -0400

    2019/03/22 setting basic UI

--- ShareLocation/ShareLocation/ViewController.swift
+++ ShareLocation/ShareLocation/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  ShareLocation
+//  FoodTracker
 //
 //  Created by Ziwen Wang on 2019/3/22.
 //  Copyright © 2019年 Ziwen Wang. All rights reserved.
@@ -10,23 +10,22 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var map: MKMapView!
+    
+    @IBOutlet weak var MKMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         var centerLocation = CLLocationCoordinate2DMake(-27, 153)
         var mapSpan = MKCoordinateSpanMake(0.01, 0.01)
         var mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
-        self.map.setRegion(mapRegion, animated: true)
-
+        self.MKMapView.setRegion(mapRegion, animated: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
